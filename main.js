25858da2503fc8909aae2c4421dbb0145239abf5
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-10-30 22:55:03 +0000

    Implement gridded layout
    Fixed dimension cards in gridded layout

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const myLibrary = [];
 
-function Book() {
-	//constructor here
-}
+function Book(title,
+	author,
+	pages,
+	haveRead) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.haveRead = haveRead;
+	this.info = function() {
+		return this.title + ", " + "by " + this.author + ", " + this.pages + " pages" + ( this.haveRead ? ", have read": ", not read yet")
+	};
+};
 
+book1 = new Book("Lord of the Rings", "J.R.R Tolkein", 999,true);
+book2 = new Book("Leviathan Wakes", "James R Corey", 1000, true);
+book3 = new Book("Cibola Burn", "James R Corey", 899, true)
+
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
 function addBookToLibrary() {
 
 }
+
+const main = document.querySelector(".main");
+
+for (book of myLibrary) {
+	const card = document.createElement("div");
+	const title = document.createElement("p");
+	const pages = document.createElement("p");
+	main.appendChild(card);
+	card.classList.add("card");
+	card.appendChild(title);
+	title.textContent = `${book.title} by ${book.author}`;
+	card.appendChild(pages);
+	pages.textContent = `${book.pages} pages`;
+	console.log(book.title, book.author, book.pages);
+};
+
