2eb4cc74c2f662a77a113e9aa32c5287ee2b10b8
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-11-12 21:00:39 +0000

    Add push to library and display library functions
    Library is populated with test data and library is displayed on page
    load.
    Errors: cancel button removes last item, <ESC> adds empty book
    object

--- main.js
+++ main.js
@@ -13,33 +13,65 @@ function Book(title,
 	};
 };
 
-book1 = new Book("Lord of the Rings", "J.R.R Tolkein", 999,true);
-book2 = new Book("Leviathan Wakes", "James R Corey", 1000, true);
-book3 = new Book("Cibola Burn", "James R Corey", 899, true)
-
-myLibrary.push(book1);
-myLibrary.push(book2);
-myLibrary.push(book3);
-
-
-function addBookToLibrary() {
-
+function addBookToLibrary(book) {
+	myLibrary.push(book)
 }
 
-const main = document.querySelector(".main");
+book1 = new Book("Lord of the Rings", "J.R.R Tolkein", 999,true);
+addBookToLibrary(book1);
+book2 = new Book("Leviathan Wakes", "James R Corey", 1000, true);
+addBookToLibrary(book2);
+book3 = new Book("Cibola Burn", "James R Corey", 899, true)
+addBookToLibrary(book3);
 
-for (book of myLibrary) {
-	
-	const card = document.createElement("div");
-	const title = document.createElement("p");
-	const author = document.createElement("p");
-	const pages = document.createElement("p");
-	const statusInfo = document.createElement("p");
-	const bookNo = myLibrary.indexOf(book) + 1;
-	const bookNoHolder = document.createElement("span");
-	console.log(bookNo);
-	main.appendChild(card);
-	card.classList.add("card");
+const addbookButton = document.getElementById("addBookButton");
+const addBookDialog = document.getElementById("addBookDialog");
+const confirmAddButton = document.getElementById("confirm-add")
+
+window.addEventListener('load', () => {
+	displayLibrary();
+});
+
+addBookButton.addEventListener("click", () => {
+	addBookDialog.showModal();
+});
+
+confirmAddButton.addEventListener("click", (e) => {
+	e.preventDefault()
+	addBookDialog.close();
+});
+
+addBookDialog.addEventListener("close", () => {
+	const formData = new FormData(addBookDialog.querySelector('form'));
+	const title = formData.get('title');
+	const author = formData.get('author');
+	const pages = formData.get('no-of-pages');
+
+	const book = new Book(title, author, pages, false)
+	addBookToLibrary(book);
+	displayLibrary();
+});
+
+
+function displayLibrary() {
+	const existingCards = document.querySelectorAll(".card");
+	if ( existingCards ) {
+		existingCards.forEach(card => {
+			card.remove();
+		})
+	};
+
+	const main = document.querySelector(".main");
+	for (book of myLibrary) {
+		const card = document.createElement("div");
+		const title = document.createElement("p");
+		const author = document.createElement("p");
+		const pages = document.createElement("p");
+		const statusInfo = document.createElement("p");
+		const bookNo = myLibrary.indexOf(book) + 1;
+		const bookNoHolder = document.createElement("span");
+		main.appendChild(card);
+		card.classList.add("card");
 		card.appendChild(title);
 		title.textContent = `${book.title}`;
 		card.appendChild(author);
@@ -51,5 +83,6 @@ for (book of myLibrary) {
 		statusInfo.appendChild(bookNoHolder);
 		statusInfo.classList.add("status-info");
 		bookNoHolder.textContent = bookNo;
+	};
 };
 
